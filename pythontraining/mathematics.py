124f23c1ff3f9dd5e5bd079fa93a7baf7cd80d23
Joris Hoendervangers <joris@hoendervangers.org>
2022-04-05 14:35:05 +0200

    demo pytest

---
+++ pythontraining/mathematics.py
@@ -0,0 +1,12 @@
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return round(x / y)
+
+
+def round_up(x):
+    result = int(x) + int((x > 0) and (x - int(x)) > 0)
+    return result
---
+++ tests/test_mathematics.py
@@ -0,0 +1,13 @@
+from pythontraining.mathematics import multiply, divide, round_up
+
+
+def test_multiply():
+    assert multiply(4, 5) == 20
+
+
+def test_divide():
+    assert divide(4, 5) == 0.8
+
+
+def test_round_up():
+    assert round_up(2.4) == 3
