82fe357fb949817f89f6f2d489faef39231082db
Joris Hoendervangers <joris@hoendervangers.org>
2022-04-06 09:39:35 +0200

    demo json

--- pythontraining/d32_json.py
+++ pythontraining/d32_json.py
@@ -2,25 +2,29 @@
 Demo of JSON module
 """
 
-
 import json
 
 person = {
     'id': 4,
-    'name': 'Peter Smith',
+    'name': 'Patricia Kennedy',
     'active': True,
-    'email': 'peter@smith.com',
-    'phone': None
+    'email': 'patricia@kennedy.org',
+    'phone': None,
 }
 
-# Output with dump or dumps (s for string)
+# Write with dump
 with open("person.json", "w") as json_file:
     json.dump(person, json_file, indent=4)
 
-# Input with load or loads (s for string)
+# Read with load
 with open("person.json", "r") as json_file:
     data = json.load(json_file)
     print(data)
 
-print(json.dumps(person))
-print(type(json.dumps(person)))
+# Write with dumps (add s for string)
+json_string = json.dumps(person, indent=4)
+print(json_string)
+
+# Read with loads (add s for string)
+data = json.loads(json_string)
+print(data)
